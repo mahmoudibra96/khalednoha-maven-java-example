e91df05c2ec7a5b7617fb1b83fa20938e38ddb10
mahmoud ibrahem <36241912+mahmoudibra96@users.noreply.github.com>
2022-02-03 11:55:26 +0200

    add me to persons
    test

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,6 +39,15 @@ public class PersonService {
 		p.setFirstName("AbdulHalim");
 		p.setLastName("Mahmoud");
 		persons.add(p);
+
+		
+		
+		p = new Person();
+		p.setId("5");
+		p.setAge(25);
+		p.setFirstName("mahmoud");
+		p.setLastName("ibrahem");
+		persons.add(p);		
 		
 	}
 	public Person getPerson(String id) {
